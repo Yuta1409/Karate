1c073a8b3e6e063763a6aa5cd49c8c635d5d4def
ROGER <yroger2003@gmail.com>
2022-01-27 10:44:54 +0100

    nouveau formulaire

--- PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_InscriptionMembre.Designer.cs
+++ PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_InscriptionMembre.Designer.cs
@@ -29,275 +29,165 @@ namespace PROJET_PPE2._1_KARATE
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_InscriptionMembre));
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Cmd_Ajouter = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.Grid_Membre = new System.Windows.Forms.DataGridView();
+            this.Grid_Comp = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numCompetition = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.num_CLUB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numLicence = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid_Membre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid_Comp)).BeginInit();
             this.SuspendLayout();
             // 
-            // button3
+            // Cmd_Ajouter
             // 
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button3.Location = new System.Drawing.Point(236, 378);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 23);
-            this.button3.TabIndex = 72;
-            this.button3.Text = "Supprimer";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(155, 378);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 71;
-            this.button2.Text = "Modifier";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(74, 378);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 70;
-            this.button1.Text = "Ajouter";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Cmd_Ajouter.Location = new System.Drawing.Point(363, 245);
+            this.Cmd_Ajouter.Name = "Cmd_Ajouter";
+            this.Cmd_Ajouter.Size = new System.Drawing.Size(75, 26);
+            this.Cmd_Ajouter.TabIndex = 70;
+            this.Cmd_Ajouter.Text = "Ajouter";
+            this.Cmd_Ajouter.UseVisualStyleBackColor = true;
+            this.Cmd_Ajouter.Click += new System.EventHandler(this.Cmd_Ajouter_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Rockwell Nova Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(64, 42);
+            this.label2.Location = new System.Drawing.Point(96, 10);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(261, 27);
+            this.label2.Size = new System.Drawing.Size(399, 27);
             this.label2.TabIndex = 69;
-            this.label2.Text = "INSCRIPTION D\'UN NOUVEAU MEMBRE";
+            this.label2.Text = "INSCRIPTION D\'UN NOUVEAU MEMBRE A UNE COMPETITION";
             // 
-            // textBox1
+            // Grid_Membre
             // 
-            this.textBox1.Location = new System.Drawing.Point(198, 100);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(131, 22);
-            this.textBox1.TabIndex = 68;
+            this.Grid_Membre.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid_Membre.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numLicence,
+            this.column,
+            this.Column2,
+            this.Column3});
+            this.Grid_Membre.Location = new System.Drawing.Point(17, 79);
+            this.Grid_Membre.Name = "Grid_Membre";
+            this.Grid_Membre.RowHeadersWidth = 51;
+            this.Grid_Membre.RowTemplate.Height = 24;
+            this.Grid_Membre.Size = new System.Drawing.Size(740, 150);
+            this.Grid_Membre.TabIndex = 71;
+            this.Grid_Membre.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Grid_Membre_CellContentClick);
             // 
-            // textBox2
+            // Grid_Comp
             // 
-            this.textBox2.Location = new System.Drawing.Point(137, 200);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(131, 22);
-            this.textBox2.TabIndex = 67;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(172, 265);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(131, 22);
-            this.textBox4.TabIndex = 66;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(198, 235);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(131, 22);
-            this.textBox3.TabIndex = 65;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(66, 105);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(134, 17);
-            this.label10.TabIndex = 64;
-            this.label10.Text = "Numéro de licence :";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(66, 240);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(134, 17);
-            this.label9.TabIndex = 63;
-            this.label9.Text = "Date de naissance :";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(66, 270);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(100, 17);
-            this.label8.TabIndex = 62;
-            this.label8.Text = "Rue / Lieu dit :";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(66, 304);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(91, 17);
-            this.label5.TabIndex = 61;
-            this.label5.Text = "Code postal :";
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(117, 334);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(131, 22);
-            this.textBox8.TabIndex = 60;
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(155, 299);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(131, 22);
-            this.textBox7.TabIndex = 59;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(112, 165);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(131, 22);
-            this.textBox6.TabIndex = 58;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(179, 133);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(131, 22);
-            this.textBox5.TabIndex = 57;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(210, 22);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(368, 398);
-            this.pictureBox1.TabIndex = 56;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(170, 280);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(0, 17);
-            this.label7.TabIndex = 55;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(66, 138);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(95, 17);
-            this.label6.TabIndex = 54;
-            this.label6.Text = "Nom du club :";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(66, 339);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 17);
-            this.label4.TabIndex = 53;
-            this.label4.Text = "Ville :";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(66, 205);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 17);
-            this.label3.TabIndex = 52;
-            this.label3.Text = "Prénom :";
+            this.Grid_Comp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid_Comp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numCompetition,
+            this.num_CLUB});
+            this.Grid_Comp.Location = new System.Drawing.Point(17, 288);
+            this.Grid_Comp.Name = "Grid_Comp";
+            this.Grid_Comp.RowHeadersWidth = 51;
+            this.Grid_Comp.RowTemplate.Height = 24;
+            this.Grid_Comp.Size = new System.Drawing.Size(740, 150);
+            this.Grid_Comp.TabIndex = 72;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 170);
+            this.label1.Location = new System.Drawing.Point(14, 268);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 17);
-            this.label1.TabIndex = 51;
-            this.label1.Text = "Nom :";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 73;
+            this.label1.Text = "Compétition";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 74;
+            this.label3.Text = "Membre";
+            // 
+            // numCompetition
+            // 
+            this.numCompetition.HeaderText = "NUM_COMPETITION";
+            this.numCompetition.MinimumWidth = 6;
+            this.numCompetition.Name = "numCompetition";
+            this.numCompetition.Width = 125;
+            // 
+            // num_CLUB
+            // 
+            this.num_CLUB.HeaderText = "NUM_CLUB";
+            this.num_CLUB.MinimumWidth = 6;
+            this.num_CLUB.Name = "num_CLUB";
+            this.num_CLUB.Width = 125;
+            // 
+            // numLicence
+            // 
+            this.numLicence.HeaderText = "NUM_LICENCE";
+            this.numLicence.MinimumWidth = 6;
+            this.numLicence.Name = "numLicence";
+            this.numLicence.Width = 125;
+            // 
+            // column
+            // 
+            this.column.HeaderText = "NUM_CLUB";
+            this.column.MinimumWidth = 6;
+            this.column.Name = "column";
+            this.column.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "NOM_MEMBRE";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "PRENOM_MEMBRE";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
             // 
             // Frm_InscriptionMembre
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(615, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(1136, 573);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.Grid_Comp);
+            this.Controls.Add(this.Grid_Membre);
+            this.Controls.Add(this.Cmd_Ajouter);
+            this.Controls.Add(this.label2);
             this.Name = "Frm_InscriptionMembre";
             this.Text = "FrmInscriptionMembre";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.Frm_InscriptionMembre_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid_Membre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid_Comp)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Cmd_Ajouter;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView Grid_Membre;
+        private System.Windows.Forms.DataGridView Grid_Comp;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numCompetition;
+        private System.Windows.Forms.DataGridViewTextBoxColumn num_CLUB;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numLicence;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
--- PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_InscriptionMembre.cs
+++ PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_InscriptionMembre.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace PROJET_PPE2._1_KARATE
 {
@@ -15,6 +16,53 @@ namespace PROJET_PPE2._1_KARATE
         public Frm_InscriptionMembre()
         {
             InitializeComponent();
+        }
+
+
+        private void Cmd_Ajouter_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Grid_Membre_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void Frm_InscriptionMembre_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conn = bdd.ConnectionBD();
+            conn.Open();
+
+            string sqlMembre = "SELECT NUM_LICENCE,NUM_CLUB, NOM_MEMBRE, PRENOM_MEMBRE FROM membre";
+            MySqlCommand cmdMembre = new MySqlCommand(sqlMembre, conn);
+            MySqlDataReader readerMembre = cmdMembre.ExecuteReader();
+
+            while (readerMembre.Read())
+            {
+                string NUM_LICENCE = (readerMembre["NUM_LICENCE"].ToString());
+                string NUM_CLUB = (readerMembre["NUM_CLUB"].ToString());
+                string NOM_MEMBRE = (readerMembre["NOM_MEMBRE"].ToString());
+                string PRENOM_MEMBRE = (readerMembre["PRENOM_MEMBRE"].ToString());
+
+                Grid_Membre.Rows.Add(NUM_LICENCE, NUM_CLUB, NOM_MEMBRE, PRENOM_MEMBRE);
+            }
+            conn.Close();
+
+            conn.Open();
+            string sqlComp = "SELECT NUM_COMPETITION, NUM_CLUB FROM competition";
+            MySqlCommand cmdComp = new MySqlCommand(sqlComp, conn);
+            MySqlDataReader readerComp = cmdComp.ExecuteReader();
+
+            while (readerComp.Read())
+            {
+                string NUM_COMPETITION = (readerComp["NUM_COMPETITION"].ToString());
+                string NUM_CLUB = (readerComp["NUM_CLUB"].ToString());
+
+                Grid_Comp.Rows.Add(NUM_COMPETITION, NUM_CLUB);
+
+            }
+            conn.Close();
         }
     }
 }
